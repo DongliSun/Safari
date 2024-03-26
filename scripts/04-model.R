25a5e8bca67fd255773c9bde943fb54ed708d1dd
DongliSun <157448492+DongliSun@users.noreply.github.com>
2024-03-26 19:32:45 -0400

    Update 04-model.R

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,11 +1,11 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models
+# Author: Dongli Sun
+# Date: 26 March 2024
+# Contact: dongli.sun@utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: 
+# Any other information needed?
 
 
 #### Workspace setup ####
